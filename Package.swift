1f0ef23d5d44e09cda90a39def0b8a4799f48f52
Mathijs Kadijk <mathijs@nonstrict.com>
2024-04-02 22:09:05 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,16 @@ import PackageDescription
 let package = Package(
     name: "RecordKit",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v13)
     ],
     products: [
-        .library(
-            name: "RecordKit",
-            targets: ["RecordKit"]),
+        .library(name: "RecordKit", targets: ["RecordKit"]),
     ],
     targets: [
         .binaryTarget(
             name: "RecordKit",
-            url: "https://download.nonstrict.eu/RecordKitBar.xcframework.zip",
-            checksum: "a610bd31ad776c67d1e815a2eda6c44487b0f98e9073e993265f021536ba5090")
+            url: "https://download.nonstrict.eu/recordkit/recordkit-swift-0.0.0.zip",
+            checksum: "a610bd31ad776c67d1e815a2eda6c44487b0f98e9073e993265f021536ba5090"
+        )
     ]
 )
