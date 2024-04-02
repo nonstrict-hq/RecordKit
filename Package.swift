4adc03ecc6e19ae2575120ee92987d865e9e5e83
Mathijs Kadijk <mathijs@nonstrict.com>
2024-04-02 12:53:16 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "RecordKit",
+    platforms: [
+        .mac(.v14)
+    ],
+    products: [
+        .library(
+            name: "RecordKit",
+            targets: ["RecordKit"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RecordKit",
+            url: "http://download.nonstrict.eu/RecordKit.xcframework.zip",
+            checksum: "10e1ee61a8ad37db920407cf7c3d699f3ad331501853c3d3c76ce87465d42ec4")
+    ]
+)
